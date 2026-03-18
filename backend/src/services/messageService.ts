7b54a36c8e702deeca5c91368a442214be5490bb
Kirill3224 <9217286@stud.kai.edu.ua>
2026-03-18 11:04:21 +0200

    feat(services): implement messageService

---
+++ backend/src/services/messageService.ts
@@ -0,0 +1,43 @@
+import {v4 as uuidv4 } from 'uuid';
+import {Message} from '../models/types';
+import {pool} from '../storage/db';
+import {validateNotEmpty} from '../validators/inputValidator';
+
+export const sendMessage = async(conversationId: string, senderId: string, text: string): Promise<Message> => {
+    validateNotEmpty(text, 'Message text');
+    validateNotEmpty(conversationId, 'Conversation ID');
+    validateNotEmpty(senderId, 'Sender ID');
+
+    const userResult = await pool.query('SELECT id FROM users WHERE id = $1', [senderId]);
+        if(userResult.rowCount === 0) throw new Error('User does not exist');
+
+    const conversationResult = await pool.query('SELECT id FROM conversations WHERE id = $1', [conversationId]);
+        if(conversationResult.rowCount === 0) throw new Error('Conversation does not exist');
+
+    const message: Message = {
+        id: uuidv4(),
+        conversationId,
+        senderId,
+        text,
+        createdAt: Date.now(),
+    };
+
+    await pool.query(
+        `INSERT INTO messages (id, "conversationId", "senderId", text, "createdAt")
+        VALUES ($1, $2, $3, $4, $5)`,
+        [message.id, message.conversationId, message.senderId, message.text, message.createdAt]
+    );
+
+    return message;
+};
+
+export const getMessage = async (conversationId: string): Promise<Message[]> => {
+    validateNotEmpty(conversationId, 'Conversation ID');
+
+    const result = await pool.query(
+        'SELECT * FROM messages WHERE "conversationId" = $1 ORDER BY "createdAt" ASC',
+        [conversationId]
+    );
+
+    return result.rows as Message[];
+};

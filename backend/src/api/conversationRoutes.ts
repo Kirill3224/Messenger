5f79df7a5dab939c0362222bf4d28c0125a66a47
Kirill3224 <9217286@stud.kai.edu.ua>
2026-03-24 16:06:00 +0200

    fix(api): rename userRouter to conversationRouter in conversationRouts controller

--- backend/src/api/conversationRoutes.ts
+++ backend/src/api/conversationRoutes.ts
@@ -3,9 +3,9 @@ import {asyncHandler} from '../utils/asyncHandler';
 import {createConversation} from '../services/conversationService';
 
 
-export const userRouter = Router();
+export const conversationRouter = Router();
 
-userRouter.post('/', asyncHandler(async(req: Request, res: Response) => {
+conversationRouter.post('/', asyncHandler(async(req: Request, res: Response) => {
     const {type} = req.body;
     const conv = await createConversation(type);
     res.status(201).json(conv);

81b154983adf76398f1d5fc9c3c24b4a0d3f517b
Kirill3224 <9217286@stud.kai.edu.ua>
2026-03-24 19:03:54 +0200

    feat: setup express application and bootstrap server

---
+++ backend/src/main.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response, NextFunction } from 'express';
+import { initDB } from './storage/db';
+import { userRouter } from './api/userRoutes';
+import { conversationRouter } from './api/conversationRoutes';
+import { messageRouter } from './api/messageRoutes';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use('/api/users', userRouter);
+app.use('/api/conversations', conversationRouter);
+app.use('/api/messages', messageRouter);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error('[Error]: ', err.message);
+
+    res.status(400).json({error: err.message});
+});
+
+const bootstrap = async () => {
+    try {
+    console.log('Connecting to database...');
+    await initDB();
+    
+    app.listen(PORT, () => {
+      console.log(`🚀 Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error('Fatal Error during startup:', error);
+    process.exit(1);
+  }
+};
+
+bootstrap();
